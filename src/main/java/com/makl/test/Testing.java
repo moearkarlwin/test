808b870dab08e7c6872560a70daf6021327aa1b2
moearkarlwin <moearkarlwin@gmail.com>
2020-01-20 09:51:13 +0630

    First Commit.

---
+++ src/main/java/com/makl/test/Testing.java
@@ -0,0 +1,12 @@
+package com.makl.test;
+
+import java.util.Scanner;
+
+public class Testing {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int i = sc.nextInt();
+        System.out.println("Entered Number is : " + i);
+    }
+}
